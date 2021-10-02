6e4e0365aa6e7621649835cce5a1d96bd8019a4e
raszion23 <rastazion23@gmail.com>
2021-10-02 19:11:16 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Raszion23 on 10/2/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
